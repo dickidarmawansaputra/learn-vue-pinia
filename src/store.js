466994a90c39a1a7c9475a26a3c187548be964c8
Dicki Darmawan Saputra <dickidarmawansaputra@gmail.com>
2025-02-28 16:19:42 +0700

    feat: store

---
+++ src/store.js
@@ -0,0 +1,6 @@
+import { defineStore } from "pinia";
+
+// biasanya penamaan identifier menggunakan useXXX, tetapi tidak wajib
+export const useCounter = defineStore("counter", () => {
+    return {};
+});
