61ec7c6d15ed5faeb591eb046a12f3a77df58d43
Dicki Darmawan Saputra <dickidarmawansaputra@gmail.com>
2025-03-01 13:31:08 +0700

    feat: action

--- src/components/Counter.vue
+++ src/components/Counter.vue
@@ -3,10 +3,17 @@ import { useCounter } from '../store';
 
 const counterStore = useCounter();
 
+// subscribe on state
 counterStore.$subscribe((mutation, state) => {
     console.info(mutation);
     console.info(state);
-})
+});
+
+// subscribe on action
+counterStore.$onAction((action, state) => {
+    console.info(action);
+    console.info(state);
+});
 
 function handleClick() {
     counterStore.counter++;
@@ -25,8 +32,12 @@ function handleReset() {
 <template>
     <div>
         <h1>Counter {{ counterStore.counter }}</h1>
-        <button @click="handleClick">Increment</button>
-        <button @click="handleReset">Reset</button>
+        <!-- <button @click="handleClick">Increment</button> -->
+         <!-- jika gunakan action -->
+        <button @click="counterStore.increment()">Increment</button>
+        <!-- <button @click="handleReset">Reset</button> -->
+         <!-- contoh gunakan action -->
+        <button @click="counterStore.reset()">Reset</button>
     </div>
 </template>
 <style scoped>
--- src/store.js
+++ src/store.js
@@ -4,7 +4,18 @@ import { ref } from "vue";
 // biasanya penamaan identifier menggunakan useXXX, tetapi tidak wajib
 export const useCounter = defineStore("counter", () => {
     const counter = ref(0);
+
+    // make action (untuk manipulasi data di dalam state)
+    function increment() {
+        counter.value++;
+    }
+
+    function reset() {
+        counter.value = 0;
+    }
     return {
-        counter
+        counter,
+        increment, // export methodnya agar bisa dipanggil
+        reset,
     };
 });
